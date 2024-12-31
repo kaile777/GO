11caf6127c98846d0bbf8c551dd40954de196683
Kaile Jones <135088331+kaile777@users.noreply.github.com>
2024-12-30 20:07:34 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"fmt"
+	"github.com/kaile777/GO-Modules/my-modules/greetings"
+)
+
+func main() {
+	// Get a greeting message and print it.
+	message := greetings.Hello("Peyton")
+	fmt.Println(message)
+}
